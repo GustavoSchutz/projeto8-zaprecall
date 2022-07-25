aa8087c39bd770fd7b7e9e8701f58eb7ad45b656
GustavoSchutz <gutofelipe8@gmail.com>
2022-07-25 00:18:28 -0300

    Finalizei a tela 3 e a obrigação de ativar apenas um card por vez, adicionei os botões da tela 4

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -8,33 +8,73 @@ const deckdata = [
     { key: "q4", title: "Pergunta 4" , question: "Podemos colocar __ dentro do JSX", answer:"expressões" }
 ];
 
-const FrontCard = ({d}) => <div className="frontCard">
-                            <p>{(d.title)}</p>
-                            <ion-icon className="questionIcon" name="play"></ion-icon>
-                        </div>;
-const BackCard = ({d}) => <div className="backCard">
-                            <div className="question">
-                                <h3>{d.question}</h3>
-                            </div>
-                            <div className="enter">
-                                <div className="buttonEnter">
-                                    <img alt="" src={setinha}/>
-                                </div>
-                            </div>
-                        </div>;
+
+
+function FrontCard({d}) {
+    return (
+        <div className="frontCard">
+            <p className="frontCardTittle">{(d.title)}</p>
+            <ion-icon className="questionIcon" name="play"></ion-icon>
+        </div>
+    )
+}
+
+function BackCard({d}) {
+
+    const [backCardVisibility, setBackCardVisibility] = useState("backCard");
+
+    const [answerCardVisibility, setAnswerCardVisibility] = useState("anwerCard hidden")
+
+    function handleClickSubmit() {
+        console.log("handelou o clique");
+        setBackCardVisibility("backCard hidden");
+        setAnswerCardVisibility("answerCard")
+    }
+
+    return (
+    <>
+        <div className={backCardVisibility}>
+            <div className="question">
+                <h3>{d.question}</h3>
+            </div>
+            <div className="enter">
+                <div onClick={() => {
+                handleClickSubmit();
+                }} className="buttonEnter">
+                    <img alt="" src={setinha}/>
+                </div>
+            </div>
+        </div>
+        <div className={answerCardVisibility}>
+            <div className="answer">
+                <h3>{d.answer}</h3>
+            </div>
+            <div className="buttons">
+                <div className="buttonMissed"><p className="paragraphButtonsSubmit">Não lembrei</p></div>
+                <div className="buttonAlmost"><p className="paragraphButtonsSubmit">Quase não lembrei</p></div>
+                <div className="buttonZap"><p className="paragraphButtonsSubmit">Zap!</p></div>
+            </div>
+        </div>
+    </>
+    )
+   
+}
+
+let cardActive = 0;
 
 
 
 function Cards({ d }) {
 
+
     const [cardClass, setCardClass] = useState("card");
 
     const [checked, setChecked] = useState(false);
 
-    function startZap() {
-        if (!checked) {
-            console.log("deu")
 
+    function startZap() {
+        if (!checked && cardActive === 0) {
+            cardActive++
             setChecked(!checked);
             activeCard();
         }
@@ -44,7 +84,6 @@ function Cards({ d }) {
     function activeCard() {
         setCardClass("card active");
     }
-
 
   return (
     <li key={d.key} className={cardClass}
